201b061874b6bd6ba398364a1088967f31d33526
Denishkunjadiya <denishkunjadiya02@gmail.com>
2025-06-16 17:58:24 +0530

    extra import are removed in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
 import "./App.css";
-import { Button } from "@shopify/polaris";
 
 import Frame_shopify from "./componet/Frame_shopify";
-
-import { TopBar, ActionList, Icon, Frame } from "@shopify/polaris";
-import { ArrowLeftIcon, QuestionCircleIcon } from "@shopify/polaris-icons";
-
-import {
-  Text,
-  InlineStack,
-  Box,
-  Card,
-  Badge,
-  BlockStack,
-  useBreakpoints,
-} from "@shopify/polaris";
-import { InfoIcon } from "@shopify/polaris-icons";
-import { useCallback } from "react";
 
 function App() {
   return (
     <>
       <Frame_shopify>Add product</Frame_shopify>
-      {/* <TopBarExample /> */}
     </>
   );
 }
