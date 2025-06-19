76c68c91c46835e448ef5ba780e8d5b01a612464
Denishkunjadiya <denishkunjadiya02@gmail.com>
2025-06-19 14:47:27 +0530

    navigation made smooth

--- src/componet/Navigation_bar.jsx
+++ src/componet/Navigation_bar.jsx
@@ -1,36 +1,47 @@
 import { Navigation, Text } from "@shopify/polaris";
+import { NavLink, useLocation, useNavigate } from "react-router";
 
 import { HomeIcon, ProductIcon } from "@shopify/polaris-icons";
+import { useEffect, useState } from "react";
 
 export default function Navigation_bar() {
+  let navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigationOnClick = (path) => {
+    navigate(path);
+  };
+
   return (
     <Navigation location="/">
       <Navigation.Section
         items={[
           {
-            url: "/Blockify-Checkout",
+            url: "#",
+            onClick: () => handleNavigationOnClick("/Blockify-Checkout"),
             label: "Blockify Checkout",
-            selected: window.location.pathname == "/Blockify-Checkout",
+            selected: location.pathname == "/Blockify-Checkout",
             icon: ProductIcon,
             subNavigationItems: [
               {
-                url: "/Checkout-rules",
-                matches: window.location.pathname == "/Checkout-rules",
+                url: "#",
+                onClick: () => handleNavigationOnClick("/Checkout-rules"),
+                matches: location.pathname == "/Checkout-rules",
                 label: "Checkout rules",
               },
               {
-                url: "/Cash-on-Delive",
-                matches: window.location.pathname == "/Cash-on-Delive",
+                url: "#",
+                onClick: () => handleNavigationOnClick("/Cash-on-Delive"),
+                matches: location.pathname == "/Cash-on-Delive",
                 label: "Cash on Delive",
               },
               {
-                url: "/Checkout",
-                matches: window.location.pathname == "/Checkout",
+                url: "#",
+                onClick: () => handleNavigationOnClick("/Checkout"),
+                matches: location.pathname == "/Checkout",
                 label: (
                   <Text
-                    fontWeight={
-                      window.location.pathname == "/Checkout" ? "bold" : ""
-                    }
+                    fontWeight={location.pathname == "/Checkout" ? "bold" : ""}
                     truncate
                   >
                     Postpurchase customization
@@ -38,23 +49,75 @@ export default function Navigation_bar() {
                 ),
               },
               {
-                url: "/Settings",
-                matches: window.location.pathname == "/Settings",
+                url: "#",
+                onClick: () => handleNavigationOnClick("/Settings"),
+                matches: location.pathname == "/Settings",
                 label: "Settings",
               },
               {
-                url: "/Pricing",
-                matches: window.location.pathname == "/Pricing",
+                url: "#",
+                onClick: () => handleNavigationOnClick("/Pricing"),
+                matches: location.pathname == "/Pricing",
                 label: "Pricing",
               },
               {
-                url: "/Analytics",
+                url: "#",
+                onClick: () => handleNavigationOnClick("/Analytics"),
                 label: "Analytics",
-                matches: window.location.pathname == "/Analytics",
+                matches: location.pathname == "/Analytics",
               },
             ],
           },
         ]}
+        // items={[
+        //   {
+        //     url: "/Blockify-Checkout",
+        //     label: "Blockify Checkout",
+        //     selected: window.location.pathname == "/Blockify-Checkout",
+        //     icon: ProductIcon,
+        //     subNavigationItems: [
+        //       {
+        //         url: "/Checkout-rules",
+        //         matches: window.location.pathname == "/Checkout-rules",
+        //         label: "Checkout rules",
+        //       },
+        //       {
+        //         url: "/Cash-on-Delive",
+        //         matches: window.location.pathname == "/Cash-on-Delive",
+        //         label: "Cash on Delive",
+        //       },
+        //       {
+        //         url: "/Checkout",
+        //         matches: window.location.pathname == "/Checkout",
+        //         label: (
+        //           <Text
+        //             fontWeight={
+        //               window.location.pathname == "/Checkout" ? "bold" : ""
+        //             }
+        //             truncate
+        //           >
+        //             Postpurchase customization
+        //           </Text>
+        //         ),
+        //       },
+        //       {
+        //         url: "/Settings",
+        //         matches: window.location.pathname == "/Settings",
+        //         label: "Settings",
+        //       },
+        //       {
+        //         url: "/Pricing",
+        //         matches: window.location.pathname == "/Pricing",
+        //         label: "Pricing",
+        //       },
+        //       {
+        //         url: "/Analytics",
+        //         label: "Analytics",
+        //         matches: window.location.pathname == "/Analytics",
+        //       },
+        //     ],
+        //   },
+        // ]}
       />
     </Navigation>
   );
